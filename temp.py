6d643a424cdd466c80786f35adfa93564f79419f
hsh5206 <about5206@naver.com>
2022-02-22 20:26:30 +0900

    temp

--- temp.py
+++ temp.py
@@ -1,19 +1,9 @@
-N = int(input())
+T = int(input())
 
-result = 1
-i = 2
-start = 1
-end = start + 6
-while True:
-    if N == 1:
-        break
-    if start < N <= end:
-        result += 1
-        break
-    start = end
-    end += 6 * i
-    i += 1
-    result += 1
-
-
-print(result)
+for _ in range(T):
+    floor, where = int(input()), int(input())
+    num = [[i for i in range(where+1)] for _ in range(floor+1)]
+    for p in range(1, len(num)):
+        for i in range(len(num[p])):
+            num[p][i] = sum(num[p-1][:i+1])
+    print(num[floor][where])
