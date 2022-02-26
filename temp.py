cf2c503b2984ca8c298e345a4ff355265700b615
hsh5206 <about5206@naver.com>
2022-02-26 21:57:29 +0900

    temp

--- temp.py
+++ temp.py
@@ -1,9 +1,3 @@
-T = int(input())
-
-for _ in range(T):
-    floor, where = int(input()), int(input())
-    num = [[i for i in range(where+1)] for _ in range(floor+1)]
-    for p in range(1, len(num)):
-        for i in range(len(num[p])):
-            num[p][i] = sum(num[p-1][:i+1])
-    print(num[floor][where])
+A, B, V = map(int, input().split())
+result = (V-B)/(A-B)
+print(int(result) if result == int(result) else int(result)+1)
