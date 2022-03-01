7d7a4989dc118a86631f66229c23aa657e6069ca
hsh5206 <about5206@naver.com>
2022-03-01 17:48:09 +0900

    add temp

--- temp.py
+++ temp.py
@@ -1,3 +1,12 @@
-A, B, V = map(int, input().split())
-result = (V-B)/(A-B)
-print(int(result) if result == int(result) else int(result)+1)
+n = int(input())
+numbers = map(int, input().split())
+sosu = 0
+for num in numbers:
+    error = 0
+    if num > 1:
+        for i in range(2, num):  # 2부터 n-1까지
+            if num % i == 0:
+                error += 1  # 2부터 n-1까지 나눈 몫이 0이면 error가 증가
+        if error == 0:
+            sosu += 1  # error가 없으면 소수.
+print(sosu)
