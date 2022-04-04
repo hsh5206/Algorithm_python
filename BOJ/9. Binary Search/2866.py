2901e0d44b959975c76a3b11ae6a7cfcf98da5fd
hsh5206 <about5206@naver.com>
2022-04-04 18:07:17 +0900

    BOJ 1Solved

---
+++ BOJ/9. Binary Search/2866.py
@@ -0,0 +1,31 @@
+# 문자열 잘라내기
+from collections import defaultdict
+R, C = map(int, input().split())
+arr = [list(input()) for _ in range(R)]
+
+result = 0
+dict = defaultdict(int)
+start, end = 0, R-1
+while start <= end:
+    mid = (start+end)//2
+
+    isOK = True
+    dict = defaultdict(int)
+    for j in range(C):
+        temp = ''
+        for i in range(mid, R):
+            temp += str(arr[i][j])
+        if not dict[temp]:
+            dict[temp] += 1
+        else:
+            isOK = False
+            break
+
+    if isOK:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+
+print(result)
