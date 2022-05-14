bc617da62c6833900291d23471499aa7911c0bb2
hsh5206 <about5206@naver.com>
2022-05-14 10:05:34 +0900

    BOJ 1solved

--- 프로그래머스/문제/Lv2/Lv2 방문 길이.py
+++ 프로그래머스/문제/Lv2/Lv2 방문 길이.py
@@ -12,7 +12,7 @@ def solution(dirs):
         if -5 <= x+dx <= 5 and -5 <= y+dy <= 5:
             road1 = ''.join(list(map(str, [x, y, x+dx, y+dy])))
             road2 = ''.join(list(map(str, [x+dx, y+dy, x, y])))
-            if road1 not in visited and road2 not in visited:
+            if road1 not in visited:
                 answer += 1
             visited.add(road1)
             visited.add(road2)
